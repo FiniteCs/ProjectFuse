59ad1150669a4e78532219092751853be767e359
Finite <remieluri@gmail.com>
2021-11-18 23:36:54 -0500

    Move precedences to SyntaxFacts

--- fc/CodeAnalysis/Parser.cs
+++ fc/CodeAnalysis/Parser.cs
@@ -2,6 +2,7 @@
 
 namespace Fuse.CodeAnalysis
 {
+
     internal sealed class Parser
     {
         private readonly SyntaxToken[] _tokens;
@@ -70,7 +71,7 @@ namespace Fuse.CodeAnalysis
 
             while (true)
             {
-                var precedence = GetBinaryOperatorPrecedence(Current.Kind);
+                var precedence = Current.Kind.GetBinaryOperatorPrecedence();
                 if (precedence == 0 || precedence <= parentPrecedence)
                     break;
 
@@ -80,23 +81,6 @@ namespace Fuse.CodeAnalysis
             }
 
             return left;
-        }
-
-        private static int GetBinaryOperatorPrecedence(SyntaxKind kind)
-        {
-            switch (kind)
-            {
-                case SyntaxKind.StarToken:
-                case SyntaxKind.SlashToken:
-                    return 2;
-
-                case SyntaxKind.PlusToken:
-                case SyntaxKind.MinusToken:
-                    return 1;
-
-                default:
-                    return 0;
-            }
         }
 
         private ExpressionSyntax ParsePrimaryExpression()
