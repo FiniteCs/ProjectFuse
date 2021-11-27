c2b165cf9d8af50301808959ebeae18bb2cb277b
Finite <remieluri@gmail.com>
2021-11-26 19:55:14 -0500

    Added SyntaxFacts.GetBinaryOperatorKinds and SyntaxFacts.GetUnaryOperatorKinds

--- Fuse/CodeAnalysis/Syntax/SyntaxFacts.cs
+++ Fuse/CodeAnalysis/Syntax/SyntaxFacts.cs
@@ -55,6 +55,26 @@
             }
         }
 
+        public static IEnumerable<SyntaxKind> GetUnaryOperatorsKinds()
+        {
+            var kinds = (SyntaxKind[])Enum.GetValues(typeof(SyntaxKind));
+            foreach (var kind in kinds)
+            {
+                if (GetUnaryOperatorPrecedence(kind) > 0)
+                    yield return kind;
+            }
+        }
+
+        public static IEnumerable<SyntaxKind> GetBinaryOperatorsKinds()
+        {
+            var kinds = (SyntaxKind[])Enum.GetValues(typeof(SyntaxKind));
+            foreach (var kind in kinds)
+            {
+                if (GetBinaryOperatorPrecedence(kind) > 0)
+                    yield return kind;
+            }
+        }
+
         public static string GetText(SyntaxKind kind)
         {
             switch (kind)
