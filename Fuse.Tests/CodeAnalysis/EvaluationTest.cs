0696a905e6dfd91d72ad7c6fc6d5fe0130bd89c6
Finite <remieluri@gmail.com>
2021-11-26 20:29:04 -0500

    Added evaluation tests

---
+++ Fuse.Tests/CodeAnalysis/EvaluationTest.cs
@@ -0,0 +1,49 @@
+﻿using Fuse.CodeAnalysis;
+using Fuse.CodeAnalysis.Syntax;
+using Xunit;
+
+namespace Fuse.Tests.CodeAnalysis.Syntax
+{
+    public class EvaluationTest
+    {
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("+1", 1)]
+        [InlineData("-1", -1)]
+        [InlineData("14 + 12", 26)]
+        [InlineData("12 - 3", 9)]
+        [InlineData("4 * 2", 8)]
+        [InlineData("9 / 3", 3)]
+        [InlineData("(10)", 10)]
+        [InlineData("12 == 3", false)]
+        [InlineData("3 == 3", true)]
+        [InlineData("12 != 3", true)]
+        [InlineData("3 != 3", false)]
+        [InlineData("false == false", true)]
+        [InlineData("true == false", false)]
+        [InlineData("false != false", false)]
+        [InlineData("true != false", true)]
+        [InlineData("true", true)]
+        [InlineData("false", false)]
+        [InlineData("!true", false)]
+        [InlineData("!false", true)]
+        [InlineData("(a = 10) * a", 100)]
+        public void SyntaxFact_GetText_RoundTrips(string text, object expectedResult)
+        {
+            var syntaxTree = SyntaxTree.Parse(text);
+            var compilation = new Compilation(syntaxTree);
+            var variables = new Dictionary<VariableSymbol, object>();
+            var result = compilation.Evaluate(variables);
+
+            Assert.Empty(result.Diagnostics);
+            Assert.Equal(expectedResult, result.Value);
+        }
+
+        public static IEnumerable<object[]> GetSyntaxKindData()
+        {
+            var kinds = (SyntaxKind[])Enum.GetValues(typeof(SyntaxKind));
+            foreach (var kind in kinds)
+                yield return new object[] { kind };
+        }
+    }
+}
