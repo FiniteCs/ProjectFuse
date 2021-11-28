10b27594a51341eccaf59917feae7c8d7d253506
Finite <remieluri@gmail.com>
2021-11-27 22:26:12 -0500

    Minor changes

--- Fuse/CodeAnalysis/Syntax/SyntaxNode.cs
+++ Fuse/CodeAnalysis/Syntax/SyntaxNode.cs
@@ -50,11 +50,9 @@ namespace Fuse.CodeAnalysis.Syntax
             writer.Write(indent);
 
             if (isToConsole)
-            {
                 Console.ForegroundColor = ConsoleColor.DarkGray;
-                writer.Write(marker);
-                Console.ResetColor();
-            }
+
+            writer.Write(marker);
 
             if (isToConsole)
                 Console.ForegroundColor = node is SyntaxToken ? ConsoleColor.Blue : ConsoleColor.Cyan;
