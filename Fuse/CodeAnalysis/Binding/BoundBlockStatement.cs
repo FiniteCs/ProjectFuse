141666eca59da43fbe3156f16f4085f85cb76c0d
Finite <remieluri@gmail.com>
2021-11-28 12:32:01 -0500

    Moved to separate files

---
+++ Fuse/CodeAnalysis/Binding/BoundBlockStatement.cs
@@ -0,0 +1,14 @@
+﻿namespace Fuse.CodeAnalysis.Binding
+{
+    internal sealed class BoundBlockStatement : BoundStatement
+    {
+        public BoundBlockStatement(ImmutableArray<BoundStatement> statements)
+        {
+            Statements = statements;
+        }
+
+        public ImmutableArray<BoundStatement> Statements { get; }
+
+        public override BoundNodeKind Kind => BoundNodeKind.BlockStatement;
+    }
+}
---
+++ Fuse/CodeAnalysis/Binding/BoundExpressionStatement.cs
@@ -0,0 +1,14 @@
+﻿namespace Fuse.CodeAnalysis.Binding
+{
+    internal sealed class BoundExpressionStatement : BoundStatement
+    {
+        public BoundExpressionStatement(BoundExpression expression)
+        {
+            Expression = expression;
+        }
+
+        public BoundExpression Expression { get; }
+
+        public override BoundNodeKind Kind => BoundNodeKind.ExpressionStatement;
+    }
+}
