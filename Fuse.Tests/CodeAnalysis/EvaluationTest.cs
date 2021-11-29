7855f55d3cc6398fa8408aa3aa30801a872fc493
Finite <remieluri@gmail.com>
2021-11-28 22:39:32 -0500

    Added tests for diagnostics

--- Fuse.Tests/CodeAnalysis/EvaluationTest.cs
+++ Fuse.Tests/CodeAnalysis/EvaluationTest.cs
@@ -30,6 +30,11 @@ namespace Fuse.Tests.CodeAnalysis.Syntax
         [InlineData("{ var a = 0 (a = 10) * a }", 100)]
         public void SyntaxFact_GetText_RoundTrips(string text, object expectedResult)
         {
+            AssertValue(text, expectedResult);
+        }
+
+        private static void AssertValue(string text, object expectedResult)
+        {
             SyntaxTree syntaxTree = SyntaxTree.Parse(text);
             Compilation compilation = new(syntaxTree);
             Dictionary<VariableSymbol, object> variables = new();
@@ -39,11 +44,121 @@ namespace Fuse.Tests.CodeAnalysis.Syntax
             Assert.Equal(expectedResult, result.Value);
         }
 
-        public static IEnumerable<object[]> GetSyntaxKindData()
+        [Fact]
+        public void Evaluator_VariableDeclaration_Reports_Redeclaration()
         {
-            SyntaxKind[] kinds = (SyntaxKind[])Enum.GetValues(typeof(SyntaxKind));
-            foreach (SyntaxKind kind in kinds)
-                yield return new object[] { kind };
+            var text =
+            @"
+                {
+                    var x = 10
+                    var y = 100
+                    {
+                        var x = 10
+                    }
+                    var [x] = 5
+                }
+            ";
+
+            var diagnostics ="Variable 'x' is already declared.";
+
+            AssetDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Name_Reports_Undefined()
+        {
+            var text = "[x] * 10";
+
+            var diagnostics = "Variable 'x' doesn't exist.";
+
+            AssetDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Assigned_Reports_Undefined()
+        {
+            var text = "[x] = 10";
+
+            var diagnostics = "Variable 'x' doesn't exist.";
+
+            AssetDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Assigned_Reports_CannotAssign()
+        {
+            var text = 
+            @"
+            {
+                let x = 10
+                x [=] 0
+            }
+            ";
+
+            var diagnostics = "Variable 'x' is read-only and cannot be assigned to.";
+
+            AssetDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Assigned_Reports_CannotConvert()
+        {
+            var text =
+            @"
+            {
+                var x = 10
+                x = [true]
+            }
+            ";
+
+            var diagnostics = "Cannot convert type 'System.Boolean' to 'System.Int32'.";
+
+            AssetDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Unary_Reports_Undefined()
+        {
+            var text = "[+]true";
+
+            var diagnostics = "Unary operator '+' is not defined for type 'System.Boolean'.";
+
+            AssetDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Binary_Reports_Undefined()
+        {
+            var text = "10 [*] false";
+
+            var diagnostics = "Binary operator '*' is not defined for types 'System.Int32' and 'System.Boolean'.";
+
+            AssetDiagnostics(text, diagnostics);
+        }
+
+        private static void AssetDiagnostics(string text, string diagnosticText)
+        {
+            var annotatedText = AnnotatedText.Parse(text);
+            var syntaxTree = SyntaxTree.Parse(annotatedText.Text);
+            var compilation = new Compilation(syntaxTree);
+            var result = compilation.Evaluate(new Dictionary<VariableSymbol, object>());
+
+            var expectedDiagnostics = AnnotatedText.UnindentLines(diagnosticText);
+            if (annotatedText.Spans.Length != expectedDiagnostics.Length)
+                throw new Exception("Must mark as many spans as there are expected diagnostics");
+
+            Assert.Equal(expectedDiagnostics.Length, result.Diagnostics.Length);
+
+            for (var i = 0; i < expectedDiagnostics.Length; i++)
+            {
+                var expectedMessage = expectedDiagnostics[i];
+                var actualMessage = result.Diagnostics[i].Message;
+                Assert.Equal(expectedMessage, actualMessage);
+
+                var expectedSpan = annotatedText.Spans[i];
+                var actualSpan = result.Diagnostics[i].Span;
+                Assert.Equal(expectedSpan, actualSpan);
+            }
         }
     }
 }
