957987b4d35e9edeb00941ecf6ad807794b213b4
Finite <remieluri@gmail.com>
2021-12-01 00:11:59 -0500

    Moved to separate files

--- Fuse/CodeAnalysis/Binding/BoundStatement.cs
+++ Fuse/CodeAnalysis/Binding/BoundStatement.cs
@@ -3,34 +3,4 @@
     internal abstract class BoundStatement : BoundNode
     { 
     }
-
-    internal sealed class BoundWhileStatement : BoundStatement
-    {
-        public BoundWhileStatement(BoundExpression condition, BoundStatement body)
-        {
-            Condition = condition;
-            Body = body;
-        }
-
-        public override BoundNodeKind Kind => BoundNodeKind.WhileStatement;
-        public BoundExpression Condition { get; }
-        public BoundStatement Body { get; }
-    }
-
-    internal sealed class BoundForStatement : BoundStatement
-    {
-        public BoundForStatement(VariableSymbol variable, BoundExpression lowerBound, BoundExpression upperBound, BoundStatement body)
-        {
-            Variable = variable;
-            LowerBound = lowerBound;
-            UpperBound = upperBound;
-            Body = body;
-        }
-
-        public override BoundNodeKind Kind => BoundNodeKind.ForStatement;
-        public VariableSymbol Variable { get; }
-        public BoundExpression LowerBound { get; }
-        public BoundExpression UpperBound { get; }
-        public BoundStatement Body { get; }
-    }
 }
