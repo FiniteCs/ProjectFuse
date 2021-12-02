7e5e5b2b2dfd25284d94f8babd9a46e83ee90d70
Finite <remieluri@gmail.com>
2021-12-01 22:19:05 -0500

    Improved the REPL
    The REPL now supports multi-line editing, syntax colorization, and history.

---
+++ fc/FuseRepl.cs
@@ -0,0 +1,136 @@
+﻿using Fuse.CodeAnalysis;
+using Fuse.CodeAnalysis.Syntax;
+using Fuse.CodeAnalysis.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fuse
+{
+    internal sealed class FuseRepl : Repl
+    {
+        private Compilation _previous;
+        private bool _showTree;
+        private bool _showProgram;
+        private readonly Dictionary<VariableSymbol, object> _variables = new();
+
+        protected override void RenderLine(string line)
+        {
+            var tokens = SyntaxTree.ParseTokens(line);
+            foreach (var token in tokens)
+            {
+                var isKeyword = token.Kind.ToString().EndsWith("Keyword");
+                var isNumber = token.Kind == SyntaxKind.NumberToken;
+                if (isKeyword)
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                else if (!isNumber)
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.Write(token.Text);
+
+                Console.ResetColor();
+            }
+        }
+
+        protected override void EvaluateMetaCommand(string input)
+        {
+            switch (input)
+            {
+                case "#showTree":
+                    _showTree = !_showTree;
+                    Console.WriteLine(_showTree ? "Showing Parse Tree." : "Not Showing Parse Tree.");
+                    break;
+                case "#showProgram":
+                    _showProgram = !_showProgram;
+                    Console.WriteLine(_showProgram ? "Showing Bound Tree." : "Not Showing Bound Tree.");
+                    break;
+                case "#cls":
+                    Console.Clear();
+                    break;
+                case "#reset":
+                    _previous = null;
+                    _variables.Clear();
+                    break;
+                default:
+                    base.EvaluateMetaCommand(input);
+                    break;
+            }
+        }
+
+        protected override void EvaluateSubmission(string text)
+        {
+            SyntaxTree syntaxTree = SyntaxTree.Parse(text);
+
+            Compilation compilation = _previous == null
+                                      ? new(syntaxTree)
+                                      : _previous.ContinueWith(syntaxTree);
+
+            if (_showTree)
+            {
+                syntaxTree.Root.WriteTo(Console.Out);
+            }
+
+            if (_showProgram)
+                compilation.EmitTree(Console.Out);
+
+            EvaluationResult result = compilation.Evaluate(_variables);
+
+            if (!result.Diagnostics.Any())
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine(result.Value);
+                Console.ResetColor();
+                _previous = compilation;
+            }
+            else
+            {
+
+                foreach (Diagnostic diagnostic in result.Diagnostics)
+                {
+                    int lineIndex = syntaxTree.Text.GetLineIndex(diagnostic.Span.Start);
+                    int lineNumber = lineIndex + 1;
+                    TextLine line = syntaxTree.Text.Lines[lineIndex];
+                    int character = diagnostic.Span.Start - line.Start + 1;
+
+                    Console.WriteLine();
+
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.Write($"({lineNumber}, {character}): ");
+                    Console.WriteLine(diagnostic);
+                    Console.ResetColor();
+
+                    TextSpan prefixSpan = TextSpan.FromBounds(line.Start, diagnostic.Span.Start);
+                    TextSpan suffixSpan = TextSpan.FromBounds(diagnostic.Span.End, line.End);
+
+                    string prefix = syntaxTree.Text.ToString(prefixSpan);
+                    string error = syntaxTree.Text.ToString(diagnostic.Span);
+                    string suffix = syntaxTree.Text.ToString(suffixSpan);
+
+                    Console.Write("    ");
+                    Console.Write(prefix);
+
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.Write(error);
+                    Console.ResetColor();
+
+                    Console.Write(suffix);
+
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        protected override bool IsCompleteSubmission(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return true;
+
+            SyntaxTree syntaxTree = SyntaxTree.Parse(text);
+            if (syntaxTree.Diagnostics.Any())
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ fc/Repl.cs
@@ -0,0 +1,387 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace Fuse
+{
+    internal abstract class Repl
+    {
+        private List<string> _submissionHistory = new();
+        protected int _submissionHistoryIndex;
+        private bool _done;
+
+        public void Run()
+        {
+            while (true)
+            {
+                var text = EditSubmission();
+                if (string.IsNullOrEmpty(text))
+                    return;
+
+                if (!text.Contains(Environment.NewLine) && text.StartsWith("#"))
+                    EvaluateMetaCommand(text);
+                else
+                    EvaluateSubmission(text);
+
+                _submissionHistory.Add(text);
+                _submissionHistoryIndex = 0;
+            }
+        }
+
+        private sealed class SubmissionView
+        {
+            private readonly Action<string> _lineRenderer;
+            private readonly ObservableCollection<string> _submissionDocument;
+            private readonly int _cursorTop;
+            private int _renderedLineCount;
+            private int _currentLine;
+            private int _curentCharacter;
+
+            public SubmissionView(Action<string> lineRenderer, ObservableCollection<string> submissionDocument)
+            {
+                _lineRenderer = lineRenderer;
+                _submissionDocument = submissionDocument;
+                submissionDocument.CollectionChanged += SubmissionDocumentChanged;
+                _cursorTop = Console.CursorTop;
+                Render();
+            }
+
+            private void SubmissionDocumentChanged(object sender, NotifyCollectionChangedEventArgs e)
+            {
+                Render();
+            }
+
+            private void Render()
+            {
+                Console.CursorVisible = false;
+
+                var lineCount = 0;
+
+                foreach (var line in _submissionDocument)
+                {
+                    Console.SetCursorPosition(0, _cursorTop + lineCount);
+                    Console.ForegroundColor = ConsoleColor.Green;
+
+                    if (lineCount == 0)
+                        Console.Write("» ");
+                    else
+                        Console.Write("· ");
+
+                    Console.ResetColor();
+                    _lineRenderer(line);
+                    Console.WriteLine(new string(' ', Console.WindowWidth - line.Length));
+                    lineCount++;
+                }
+
+                var numberOfBlankLines = _renderedLineCount - lineCount;
+                if (numberOfBlankLines > 0)
+                {
+                    var blankLine = new string(' ', Console.WindowWidth);
+                    for (var i = 0; i < numberOfBlankLines; i++)
+                    {
+                        Console.SetCursorPosition(0, _cursorTop + lineCount + i);
+                        Console.WriteLine(blankLine);
+                    }
+                }
+
+                _renderedLineCount = lineCount;
+                Console.CursorVisible = true;
+                UpdateCursorPosition();
+            }
+
+            private void UpdateCursorPosition()
+            {
+                Console.CursorTop = _cursorTop + _currentLine;
+                Console.CursorLeft = 2 + CurentCharacter;
+            }
+
+            public int CurrentLine 
+            { 
+                get => _currentLine;
+                set
+                {
+                    if (_currentLine != value)
+                    {
+                        _currentLine = value;
+                        _curentCharacter = Math.Min(_submissionDocument[_currentLine].Length, _curentCharacter);
+                        UpdateCursorPosition();
+                    }
+                }
+            }
+
+            public int CurentCharacter 
+            { 
+                get => _curentCharacter;
+                set
+                {
+                    if (_curentCharacter != value)
+                    {
+                        _curentCharacter = value;
+                        UpdateCursorPosition();
+                    }
+                } 
+            }
+        }
+
+        private string EditSubmission()
+        {
+            _done = false;
+
+            var document = new ObservableCollection<string>() { "" };
+            var view = new SubmissionView(RenderLine, document);
+
+            while (!_done)
+            {
+                var key = Console.ReadKey(true);
+                HandleKey(key, document, view);
+            }
+
+            view.CurrentLine = document.Count - 1;
+            view.CurentCharacter = document[view.CurrentLine].Length;
+            Console.WriteLine();
+
+            return string.Join(Environment.NewLine, document);
+        }
+
+        private void HandleKey(ConsoleKeyInfo key, ObservableCollection<string> document, SubmissionView view)
+        {
+            if (key.Modifiers == default)
+            {
+                switch (key.Key)
+                {
+                    case ConsoleKey.Escape:
+                        HandleEscape(document, view);
+                        break;
+                    case ConsoleKey.Enter:
+                        HandleEnter(document, view);
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        HandleLeftArrow(document, view);
+                        break;
+                    case ConsoleKey.RightArrow:
+                        HandleRightArrow(document, view);
+                        break;
+                    case ConsoleKey.UpArrow:
+                        HandleUpArrow(document, view);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        HandleDownArrow(document, view);
+                        break;
+                    case ConsoleKey.Backspace:
+                        HandleBackspace(document, view);
+                        break;
+                    case ConsoleKey.Delete:
+                        HandleDelete(document, view);
+                        break;
+                    case ConsoleKey.Home:
+                        HandleHome(document, view);
+                        break;
+                    case ConsoleKey.End:
+                        HandleEnd(document, view);
+                        break;
+                    case ConsoleKey.Tab:
+                        HandleTab(document, view);
+                        break;
+                    case ConsoleKey.PageUp:
+                        HandlePageUp(document, view);
+                        break;
+                    case ConsoleKey.PageDown:
+                        HandlePageDown(document, view);
+                        break;
+                }
+            }
+            else if (key.Modifiers == ConsoleModifiers.Control)
+            {
+                switch (key.Key)
+                {
+                    case ConsoleKey.Enter:
+                        HandleControlEnter(document, view);
+                        break;
+                }
+            }
+
+            if (key.KeyChar >= ' ')
+                HandleTyping(document, view, key.KeyChar.ToString());
+        }
+
+        private void HandleEscape(ObservableCollection<string> document, SubmissionView view)
+        {
+            document[view.CurrentLine] = String.Empty;
+            view.CurentCharacter = 0;
+        }
+
+        private void HandleEnter(ObservableCollection<string> document, SubmissionView view)
+        {
+            var submissionText = string.Join(Environment.NewLine, document);
+            if (submissionText.StartsWith("#") || IsCompleteSubmission(submissionText))
+            {
+                _done = true;
+                return;
+            }
+
+            InsertLine(document, view);
+        }
+
+        private void HandleControlEnter(ObservableCollection<string> document, SubmissionView view)
+        {
+            InsertLine(document, view);
+        }
+
+        private static void InsertLine(ObservableCollection<string> document, SubmissionView view)
+        {
+            var remainder = document[view.CurrentLine].Substring(view.CurentCharacter);
+            document[view.CurrentLine] = document[view.CurrentLine].Substring(0, view.CurentCharacter);
+
+            var lineIndex = view.CurrentLine + 1;
+            document.Insert(lineIndex, remainder);
+            view.CurentCharacter = 0;
+            view.CurrentLine = lineIndex;
+        }
+
+        private void HandleLeftArrow(ObservableCollection<string> document, SubmissionView view)
+        {
+            if (view.CurentCharacter > 0)
+                view.CurentCharacter--;
+        }
+
+        private void HandleRightArrow(ObservableCollection<string> document, SubmissionView view)
+        {
+            var line = document[view.CurrentLine];
+            if (view.CurentCharacter <= line.Length - 1)
+                view.CurentCharacter++;
+        }
+
+        private void HandleUpArrow(ObservableCollection<string> document, SubmissionView view)
+        {
+            if (view.CurrentLine > 0)
+                view.CurrentLine--;
+        }
+
+        private void HandleDownArrow(ObservableCollection<string> document, SubmissionView view)
+        {
+            if (view.CurrentLine < document.Count - 1)
+                view.CurrentLine++;
+        }
+
+        private void HandleBackspace(ObservableCollection<string> document, SubmissionView view)
+        {
+            var start = view.CurentCharacter;
+            if (start == 0)
+            {
+                if (view.CurrentLine == 0)
+                    return;
+
+                var currentLine = document[view.CurrentLine];
+                var previousLine = document[view.CurrentLine - 1];
+                document.RemoveAt(view.CurrentLine);
+                view.CurrentLine--;
+                document[view.CurrentLine] = previousLine + currentLine;
+                view.CurentCharacter = previousLine.Length;
+                return;
+            }
+            else
+            {
+                var lineIndex = view.CurrentLine;
+                var line = document[lineIndex];
+                var before = line.Substring(0, start - 1);
+                var after = line.Substring(start);
+                document[lineIndex] = before + after;
+                view.CurentCharacter--;
+            }
+        }
+
+        private void HandleDelete(ObservableCollection<string> document, SubmissionView view)
+        {
+            var lineIndex = view.CurrentLine;
+            var line = document[lineIndex];
+            var start = view.CurentCharacter;
+            if (start >= line.Length)
+                return;
+
+            var before = line.Substring(0, start);
+            var after = line.Substring(start + 1);
+            document[lineIndex] = before + after;
+        }
+
+        private void HandleHome(ObservableCollection<string> document, SubmissionView view)
+        {
+            view.CurentCharacter = 0;
+        }
+
+        private void HandleEnd(ObservableCollection<string> document, SubmissionView view)
+        {
+            view.CurentCharacter = document[view.CurrentLine].Length;
+        }
+
+        private void HandleTab(ObservableCollection<string> document, SubmissionView view)
+        {
+            const int TabWidth = 4;
+            var start = view.CurentCharacter;
+            var remainingSpaces = TabWidth - start % TabWidth;
+            var line = document[view.CurrentLine];
+            document[view.CurrentLine] = line.Insert(start, new string(' ', remainingSpaces));
+            view.CurentCharacter += remainingSpaces;
+        }
+
+        private void HandlePageUp(ObservableCollection<string> document, SubmissionView view)
+        {
+            _submissionHistoryIndex--;
+            if (_submissionHistoryIndex < 0)
+                _submissionHistoryIndex = _submissionHistory.Count - 1;
+
+            UpdateDocumentFromHistory(document, view);
+        }
+
+        private void HandlePageDown(ObservableCollection<string> document, SubmissionView view)
+        {
+            _submissionHistoryIndex++;
+            if (_submissionHistoryIndex > _submissionHistory.Count - 1)
+                _submissionHistoryIndex = 0;
+
+            UpdateDocumentFromHistory(document, view);
+        }
+
+        private void UpdateDocumentFromHistory(ObservableCollection<string> document, SubmissionView view)
+        {
+            document.Clear();
+
+            var historyItme = _submissionHistory[_submissionHistoryIndex];
+            var lines = historyItme.Split(Environment.NewLine);
+            foreach (var line in lines)
+                document.Add(line);
+
+            view.CurrentLine = document.Count - 1;
+            view.CurentCharacter = document[view.CurrentLine].Length;
+        }
+
+        private void HandleTyping(ObservableCollection<string> document, SubmissionView view, string text)
+        {
+            var lineIndex = view.CurrentLine;
+            var start = view.CurentCharacter;
+            document[lineIndex] = document[lineIndex].Insert(start, text);
+            view.CurentCharacter += text.Length;
+        }
+
+        protected void ClearHistory()
+        {
+            _submissionHistory.Clear();
+        }
+
+        protected virtual void RenderLine(string line)
+        {
+            Console.Write(line);
+        }
+
+        protected virtual void EvaluateMetaCommand(string input)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Invalid command {input}.");
+            Console.ResetColor();
+        }
+
+        protected abstract void EvaluateSubmission(string text);
+
+        protected abstract bool IsCompleteSubmission(string text);
+    }
+}
